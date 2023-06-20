1e305c16fe158e878bd62c945085e69404e37de5
CodingLime <mr.marcelcarvalho@gmail.com>
2023-06-20 15:00:02 +0100

    Initial Commit

---
+++ Sweet Desserts SwiftUI/Sweet_Desserts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet_Desserts_SwiftUIApp.swift
+//  Sweet Desserts SwiftUI
+//
+//  Created by Marcel Carvalho on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Sweet_Desserts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
